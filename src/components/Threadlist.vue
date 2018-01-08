d893d4351e815d75e3d96eb83b9f25397ba9cc54
taskym <sexcnerd@gmail.com>
2018-01-07 23:25:36 -0600

    Testing compenents

---
+++ src/components/Threadlist.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+      <input id="threadlist" name="threadlist" :value="this.msg">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Threadlist',
+    msg: 'Shit',
+    props: {
+      msg: String
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Threadlist from '@/components/Threadlist'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/thread',
+      path: '/',
       name: 'Hello',
       component: HelloWorld
+    },
+    {
+      path: '/Threadlist',
+      name: 'Threadlist',
+      component: Threadlist
     }
   ]
 })
